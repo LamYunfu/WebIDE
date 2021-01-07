bb3357e6b0f70865166f5590a548d3dce317227f
204 s <liangfuqi2014@qq.com>
2021-01-07 16:25:21 +0800

    modify

--- clean.sh
+++ clean.sh
@@ -2,6 +2,7 @@ echo "**/*.ts">.yarnclean
 echo "**/*.ts.map">>.yarnclean
 echo "**/*.spec.*" >>.yarnclean
 echo "**/*.zip" >>.yarnclean 
+
 yarn autoclean --force
 find   -maxdepth 2 -type d -name src |xargs rm -r
 rm .yarnclean 
